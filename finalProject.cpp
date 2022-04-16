b3869dd43c29a08d038aa29ef441e088d83526e5
Airick73 <72322466+Airick73@users.noreply.github.com>
2022-04-16 17:45:31 -0400

    procedural code w/ backtracking, needs termination

--- finalProject.cpp
+++ finalProject.cpp
@@ -2,72 +2,114 @@
 using namespace std;
 #define SIZE 5
 //the maze problem
-int maze[SIZE][SIZE] = {
+
+
+
+int main() {
+   int maze[SIZE][SIZE] = {
    {0,0,0,0,0},
    {0,0,0,0,0},
    {0,0,0,0,0},
    {0,0,0,1,1},
    {0,0,0,0,0}
-};
-//matrix to store the solution
-int solution[SIZE][SIZE];
-//function to print the solution matrix
-void printsolution() {
-   int i,j;
-   for(i=0;i<SIZE;i++) {
-      for(j=0;j<SIZE;j++) {
+   };
+
+   int solution[SIZE][SIZE];
+
+   for(int i=0; i<SIZE; i++) {
+      for(int j=0; j<SIZE; j++) {
+         solution[i][j] = 0;
+      }
+   }
+
+   cout << "maze at start: \n";
+   for(int i=0;i<SIZE;i++) {
+      for(int j=0;j<SIZE;j++) {
+         printf("%d\t",maze[i][j]);
+      }
+      printf("\n\n");
+   }
+
+   cout << "solution at start: \n";
+   for(int i=0;i<SIZE;i++) {
+      for(int j=0;j<SIZE;j++) {
          printf("%d\t",solution[i][j]);
       }
       printf("\n\n");
    }
-}
-//function to solve the maze
-//using backtracking
-int solvemaze(int r, int c) {
-   //if destination is reached, maze is solved
-   //destination is the last cell(maze[SIZE-1][SIZE-1])
-   if((r==0) && (c==4)) {  //set destination here
-      solution[r][c] = 1;
-      return 1;
+
+
+
+   int r = 4, c = 0; //set starting point
+   cout << "starting coordinates: r = " << r << " c = " << c << "\n";
+   int flag;
+   int lastMove;
+
+   while((r != 0) || (c != 4)){ //set destination here
+         solution[r][c] = 1;
+         flag = 0;
+
+         //going right
+         if(c + 1 >= 0 && c + 1 < SIZE && solution[r][c+1] == 0 && maze[r][c+1] == 0 && flag == 0){
+            c = c + 1;
+            cout << "Moving right...\n";
+            flag = 1;
+            lastMove = 0;
+         }
+         //going up
+         if(r - 1 >= 0 && r - 1<SIZE && solution[r-1][c] == 0 && maze[r-1][c] == 0 && flag == 0){
+            r = r - 1;
+            cout << "Moving up...\n";
+            flag = 1;
+            lastMove = 1;
+         }
+
+         //going down
+         if(r+1 >= 0 && r + 1 < SIZE && solution[r+1][c] == 0 && maze[r+1][c] == 0 && flag == 0){ 
+            r = r + 1;
+            cout << "Moving down...\n";
+            flag = 1;
+            lastMove = 2;
+         }
+
+         //going left 
+         if(c - 1 >=0 && c - 1<SIZE && solution[r][c-1] == 0 && maze[r][c-1] == 0 && flag == 0){
+            c = c - 1;
+            cout << "Moving left...\n";
+            flag = 1;
+            lastMove = 3;
+         }
+
+         if(flag == 0){ //if no moves can be made mark the cell as blocked and reverse the last move made
+            maze[r][c] = 1;
+            if(lastMove == 0){
+               c = c - 1;
+            }
+            else if(lastMove == 1){
+               r = r + 1;
+            }
+            else if(lastMove == 2){
+               r = r - 1;
+            }
+            else{
+               c = c + 1;
+            }
+         }
+         
+      
    }
-   //checking if we can visit in this cell or not
-   //the indices of the cell must be in (0,SIZE-1)
-   //and solution[r][c] == 0 is making sure that the cell is not already visited
-   //maze[r][c] == 0 is making sure that the cell is not blocked
-   if(r>=0 && c>=0 && r<SIZE && c<SIZE && solution[r][c] == 0 && maze[r][c] == 0){
-      //if safe to visit then visit the cell
-      solution[r][c] = 1;
-      //going down
-      if(solvemaze(r+1, c)) 
-         return 1;
-      //going right
-      if(solvemaze(r, c+1))
-         return 1;
-      //going up
-      if(solvemaze(r-1, c)) 
-         return 1;
-      //going left 
-      if(solvemaze(r, c-1)) 
-         return 1;
-      //backtracking
-      solution[r][c] = 0;
-      return 0;
-   }
-   return 0;
-}
-int main() {
-   //making all elements of the solution matrix 0
-   int i,j;
-   for(i=0; i<SIZE; i++) {
-      for(j=0; j<SIZE; j++) {
-         solution[i][j] = 0;
+   solution[r][c] = 1;
+
+   //code to print solution
+   for(int i=0;i<SIZE;i++) {
+      for(int j=0;j<SIZE;j++) {
+         printf("%d\t",solution[i][j]);
       }
+      printf("\n\n");
    }
-   if (solvemaze(4,0)) // input robot starting destination 
-      printsolution();
-   else
-      printf("No solution\n");
+
    return 0;
+
 }
 
 
@@ -76,73 +118,3 @@ int main() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// #include <stdio.h>
-
-// struct cell{
-// 	int ob;
-// 	int xCord;
-// 	int yCord;
-// };
-
-// int main()
-// {
-// 	int dimX = 5, dimY = 5; //dimensions of the board
-// 	int ob; //obstiacle 
-// 	//target has corrdinates xt, yt
-// 	int goalX = 0, goalY = 4; //goal is set to top right
-// 	int robotX = 4, robotY = 0; //robot is set to bottom left
-// 	int goalReached = 0;
-// 						//x =  0, 1, 2, 3, 4
-// 	int maze[dimX][dimY] = { { 0, 0, 0, 0, 0 },  //y = 0
-// 					         { 0, 0, 0, 0, 0 },  //y = 1
-// 					         { 0, 0, 0, 0, 0 },  //y = 2
-// 					         { 0, 0, 0, 0, 0 },  //y = 3
-// 							 { 0, 0, 0, 0, 0 }}; //y = 4
-// 	maze[goalX][goalY] = 3;
-
-// 	int mazeSol[dimX][dimY]; // matrix to store the solution 
-
-// 	//loop to find destination in top right
-// 	//while(goal is not reached){
-// 	//	try to move down  // x 
-// 	//	try to move left
-// 	//	try to move right
-// 	//	try to move up
-// 	// 	at each move check to see if the move is valid (inside the board and does not move into wall)
-// 	// 	if it is valid make the move and check if the goal is reached
-// 	//}
-
-// 	while(goalReached == 0){
-// 		if(maze[robotX][robotY])
-// 	}
-
-
-
-
-
-
-	
-// 	return 0;
-// }
